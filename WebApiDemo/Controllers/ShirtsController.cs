d7be99a29d7ff6fbe3a4811cc08f59ae795cd7df
dorijantomic <dorijan11@gmail.com>
2024-01-07 19:35:17 +0100

    initial commit

---
+++ WebApiDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemo.Models;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShirtsController: ControllerBase
+    {
+        [HttpGet]
+        public string GetShirts()
+        {
+            return "Reading all the shirts";
+        }
+        [HttpGet("{id}")]
+        public string GetShirtsById(int id)
+        {
+            return $"Reading shirt: {id}";
+        }
+        [HttpPost]
+        public string CreateShirt([FromBody]ShirtModel shirt)
+        {
+            return "Creating a shirt";
+        }
+        [HttpPut("{id}")]
+        public string UpdateShirt(int id)
+        {
+            return $"Updating shirt: {id}";
+        }
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id) {
+            return $"Deleting shirt: {id}";
+        }
+    }
+}
---
+++ WebApiDemo/Models/ShirtModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApiDemo.Models
+{
+    public class ShirtModel
+    {
+        public int ShirtId { get; set; }
+        [Required]
+        public string? Brand { get; set; }
+        [Required]
+        public string? Colour { get; set; }
+        public int? Size { get ;set; }
+        [Required]
+        public string? Gender { get; set; }
+
+        public double? Price { get; set; }
+
+
+    }
+}
